311fcf103717f43c7ad2ae6b0c303b0d8d8f1953
nathsuki73 <nathsegovia73@gmail.com>
2025-05-22 00:14:43 +0800

    feat: add CatYapper component for dynamic cat messages

---
+++ src/components/CatYapper.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState, useCallback } from "react";
+import React from "react";
+import { debounce } from "lodash";
+import Link from "next/link";
+
+type CatYapperProps = {
+  text: string;
+};
+
+const CatYapper = ({ text }: CatYapperProps) => {
+  const [CatTalks, setCatTalks] = useState("");
+
+  const getYap = async (text: string) => {
+    const res = await fetch("http://127.0.0.1:8000/let-cat-yap", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text }),
+    });
+    console.log("here");
+
+    const data = await res.json();
+    setCatTalks(data.catYap);
+  };
+
+  const debouncedYap = debounce((t: string) => {
+    getYap(t);
+  }, 1000);
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout | null = null;
+
+    if (text.trim() === "") {
+      interval = setInterval(() => {
+        getYap("");
+      }, 10000);
+    } else {
+      debouncedYap(text);
+    }
+
+    return () => {
+      if (interval) clearInterval(interval);
+      debouncedYap.cancel();
+    };
+  }, [text]);
+
+  return (
+    <div className="text-lg text-yellow-600 italic font-light tracking-wide mt-4 transition-all duration-300">
+      "{CatTalks}"
+    </div>
+  );
+};
+
+export default CatYapper;
