4db6df2ad40cb93309dbf6d04b7dec0358d687cc
Tan <nathsegovia73@gmail.com>
2025-05-31 15:10:58 +0800

    fix(package-lock): update project name to 'untold-diaries' in package-lock.json fix(route): remove unused import of NextApiRequest and NextApiResponse

--- src/app/api/turnstile/submit/route.ts
+++ src/app/api/turnstile/submit/route.ts
@@ -1,4 +1,3 @@
-import type { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
 const verifyCaptcha = async (token: string): Promise<boolean> => {
